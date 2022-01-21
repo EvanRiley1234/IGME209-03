0a726e90c5c599e51d99bc474cc4551e6b464d02
evan riley <uluruecr@outlook.com>
2022-01-21 02:32:16 -0500

    PE - 3 finished

---
+++ PE 3 - Functions/PE 3 - Functions/PE 3 - Functions.cpp
@@ -0,0 +1,10 @@
+// PE 3 - Functions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "functions.h"
+
+int main()
+{
+    PrintName();
+}
---
+++ PE 3 - Functions/PE 3 - Functions/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+void PrintName()
+{
+    std::cout << "My Name is Evan Riley\n" << std::endl;
+}
